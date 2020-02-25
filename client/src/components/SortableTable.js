13bcfddedfaed73aaf40bb826a653ebc6f3730c4
matthew.outen <matthew.outen@matthew-outen-mbp.local>
2020-02-25 08:16:34 -0600

    fixed link. added rest of columns

--- client/src/components/SortableTable.js
+++ client/src/components/SortableTable.js
@@ -1,99 +1,139 @@
-import _ from "lodash";
-import React, { Component } from "react";
-import { Table, Pagination } from "semantic-ui-react";
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Table, Pagination } from 'semantic-ui-react';
 
 export default class SortableTable extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      column: null,
-      playerData: [""],
-      activePage: 1,
-      currentVisibleData: [""],
-      direction: null
-    };
-  }
+		this.state = {
+			column: null,
+			playerData: [ '' ],
+			activePage: 1,
+			currentVisibleData: [ '' ],
+			direction: null
+		};
+	}
 
-  componentDidMount() {
-    let playerData = this.props.playerData;
-    let currentVisibleData = this.props.currentVisibleData;
-    this.setState({ currentVisibleData, playerData });
-  }
+	componentDidMount() {
+		let playerData = this.props.playerData;
+		console.log(playerData);
+		let currentVisibleData = this.props.currentVisibleData;
+		this.setState({ currentVisibleData, playerData });
+	}
 
-  componentDidUpdate(prevProps) {
-    // Typical usage
-    if (this.props.currentVisibleData !== prevProps.currentVisibleData) {
-      this.setState({ currentVisibleData: this.props.currentVisibleData });
-    }
-  }
+	componentDidUpdate(prevProps) {
+		// Typical usage
+		if (this.props.currentVisibleData !== prevProps.currentVisibleData) {
+			this.setState({ currentVisibleData: this.props.currentVisibleData });
+		}
+	}
 
-  handleSort = clickedColumn => () => {
-    const { column, currentVisibleData, direction } = this.state;
+	handleSort = (clickedColumn) => () => {
+		const { column, currentVisibleData, direction } = this.state;
 
-    if (column !== clickedColumn) {
-      this.setState({
-        column: clickedColumn,
-        currentVisibleData: _.sortBy(currentVisibleData, [clickedColumn]),
-        direction: "ascending"
-      });
-      console.log(currentVisibleData);
-      return;
-    }
+		if (column !== clickedColumn) {
+			this.setState({
+				column: clickedColumn,
+				currentVisibleData: _.sortBy(currentVisibleData, [ clickedColumn ]),
+				direction: 'ascending'
+			});
+			console.log(currentVisibleData);
+			return;
+		}
 
-    this.setState({
-      currentVisibleData: currentVisibleData.reverse(),
-      direction: direction === "ascending" ? "descending" : "ascending"
-    });
-  };
+		this.setState({
+			currentVisibleData: currentVisibleData.reverse(),
+			direction: direction === 'ascending' ? 'descending' : 'ascending'
+		});
+	};
 
-  render() {
-    const { column, direction, currentVisibleData } = this.state;
+	render() {
+		const { column, direction, currentVisibleData } = this.state;
 
-    return (
-      <div>
-        <Pagination
-          defaultActivePage={this.props.activePage || 1}
-          totalPages={this.props.totalPages || 1}
-          onPageChange={this.props.onPageChange}
-        />
+		return (
+			<div>
+				<Pagination
+					defaultActivePage={this.props.activePage || 1}
+					totalPages={this.props.totalPages || 1}
+					onPageChange={this.props.onPageChange}
+				/>
 
-        <Table sortable celled fixed style={{ marginTop: "20px" }}>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell
-                sorted={column === "name" ? direction : null}
-                onClick={this.handleSort("name")}
-              >
-                Name
-              </Table.HeaderCell>
-              <Table.HeaderCell
-                sorted={column === "rank" ? direction : null}
-                onClick={this.handleSort("rank")}
-              >
-                Rank
-              </Table.HeaderCell>
-              <Table.HeaderCell
-                sorted={column === "position" ? direction : null}
-                onClick={this.handleSort("position")}
-              >
-                Position
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {currentVisibleData.map(({ name, rank, position }, idx) => {
-              return (
-                <Table.Row key={idx}>
-                  <Table.Cell>{name}</Table.Cell>
-                  <Table.Cell>{rank}</Table.Cell>
-                  <Table.Cell>{position}</Table.Cell>
-                </Table.Row>
-              );
-            })}
-          </Table.Body>
-        </Table>
-      </div>
-    );
-  }
+				<Table sortable celled fixed style={{ marginTop: '20px' }}>
+					<Table.Header>
+						<Table.Row>
+							<Table.HeaderCell
+								sorted={column === 'name' ? direction : null}
+								onClick={this.handleSort('name')}
+							>
+								Name
+							</Table.HeaderCell>
+							<Table.HeaderCell
+								sorted={column === 'rank' ? direction : null}
+								onClick={this.handleSort('rank')}
+							>
+								Rank
+							</Table.HeaderCell>
+							<Table.HeaderCell
+								sorted={column === 'position' ? direction : null}
+								onClick={this.handleSort('position')}
+							>
+								Position
+							</Table.HeaderCell>
+							<Table.HeaderCell
+								sorted={column === 'bye' ? direction : null}
+								onClick={this.handleSort('bye')}
+							>
+								Bye
+							</Table.HeaderCell>
+							<Table.HeaderCell
+								sorted={column === 'best' ? direction : null}
+								onClick={this.handleSort('best')}
+							>
+								Best
+							</Table.HeaderCell>
+							<Table.HeaderCell
+								sorted={column === 'worst' ? direction : null}
+								onClick={this.handleSort('worst')}
+							>
+								Worst
+							</Table.HeaderCell>
+							<Table.HeaderCell
+								sorted={column === 'average' ? direction : null}
+								onClick={this.handleSort('average')}
+							>
+								Average
+							</Table.HeaderCell>
+							<Table.HeaderCell
+								sorted={column === 'link' ? direction : null}
+								onClick={this.handleSort('link')}
+							>
+								Player News
+							</Table.HeaderCell>
+						</Table.Row>
+					</Table.Header>
+					<Table.Body>
+						{currentVisibleData.map(({ name, rank, position, bye, best, worst, average, link }, idx) => {
+							return (
+								<Table.Row key={idx}>
+									<Table.Cell>{name}</Table.Cell>
+									<Table.Cell>{rank}</Table.Cell>
+									<Table.Cell>{position}</Table.Cell>
+									<Table.Cell>{bye}</Table.Cell>
+									<Table.Cell>{best}</Table.Cell>
+									<Table.Cell>{worst}</Table.Cell>
+									<Table.Cell>{average}</Table.Cell>
+									<Table.Cell>
+										<a href={link} target="_blank">
+											Click Here
+										</a>
+									</Table.Cell>
+								</Table.Row>
+							);
+						})}
+					</Table.Body>
+				</Table>
+			</div>
+		);
+	}
 }
--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,39 +1,37 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 module.exports = {
-  index(req, res, next) {
-    const url = process.env.FULL_LINK;
-    let playerData = [];
-    axios
-      .get(url)
-      .then(response => {
-        getData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+	index(req, res, next) {
+		const url = process.env.FULL_LINK;
+		let playerData = [];
+		axios
+			.get(url)
+			.then((response) => {
+				getData(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 
-    function getData(html) {
-      const $ = cheerio.load(html);
-      $("table.player-table tr.player-row").each((i, elem) => {
-        let rank = $($(elem).find("td")[0]).text();
-        playerData.push({
-          name: $(elem)
-            .find("td a span.full-name")
-            .text(),
-          rank: Number(rank),
-          position: $($(elem).find("td")[3]).text(),
-          bye: $($(elem).find("td")[4]).text(),
-          best: $($(elem).find("td")[5]).text(),
-          worst: $($(elem).find("td")[6]).text(),
-          average: $($(elem).find("td")[7]).text(),
-          link: `${process.env.SHORT_LINK}/${$($(elem).find("td")[2])
-            .find("a")
-            .attr("href")}`
-        });
-      });
-      res.send(playerData);
-    }
-  }
+		function getData(html) {
+			const $ = cheerio.load(html);
+			$('table.player-table tr.player-row').each((i, elem) => {
+				let link = `${process.env.SHORT_LINK}/${$($(elem).find('td')[2]).find('a').attr('href')}`;
+				let nameURL = link.split('/').pop().split('.').shift();
+				playerData.push({
+					name: $(elem).find('td a span.full-name').text(),
+					rank: Number($($(elem).find('td')[0]).text()),
+					position: $($(elem).find('td')[3]).text(),
+					bye: $($(elem).find('td')[4]).text(),
+					best: Number($($(elem).find('td')[5]).text()),
+					worst: Number($($(elem).find('td')[6]).text()),
+					average: Number($($(elem).find('td')[7]).text()),
+					link: `${process.env.SHORT_LINK}/nfl/players/${nameURL}.php`,
+					nameURL: nameURL
+				});
+			});
+			res.send(playerData);
+		}
+	}
 };
